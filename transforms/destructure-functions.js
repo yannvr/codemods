9fd2c3ada0dfcb6b1b6ddc9b119ca5a1d0682dd1
Yann VR <yannvr@gmail.com>
2018-11-29 00:01:36 +0000

    feat(function) find all functions

--- transforms/destructure-functions.js
+++ transforms/destructure-functions.js
@@ -1,8 +1,6 @@
 /*
   Destructure functions having state or props objects as sole argument
   Options:
-    --arrow=1 arrow function destructuring
-    --decl=1 transform FunctionDeclaration rather than FunctionExpression
     --state=1 property name for destructuring (default: props)
 
   FunctionDeclaration:
@@ -25,19 +23,9 @@ module.exports = function(file, api, options) {
   const j = api.jscodeshift
   const root = j(file.source)
   let propToReplace
-  // FunctionExpression for class methods and FunctionDeclaration for other
-  const functionType = options.arrow ? "ArrowFunction" : "Function"
-  const expressionOrDeclaration = options.decl ? "Declaration" : "Expression"
-  const _functionType = functionType + expressionOrDeclaration
 
   const propName = options.state ? "state" : "props"
 
-  if (!j[_functionType]) {
-    console.error("Invalid arguments")
-    return
-  }
-
-  // console.log("function type", functionType)
   // console.log("prop name", propName)
 
   const hasFunctionParams = (paramName, p) =>
@@ -62,7 +50,7 @@ module.exports = function(file, api, options) {
   }
 
   return root
-    .find(j[_functionType])
+    .find(j.Function)
     .filter(p => hasFunctionParams(propName, p))
     .forEach(p => replaceParamWithProps(p))
     .toSource({ trailingComma: true })
